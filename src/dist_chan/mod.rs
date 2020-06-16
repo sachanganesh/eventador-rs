dd70a2e9668da787b7fbaea625ce5e6136ef4113
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2020-06-16 11:25:30 -0700

    working send and recv of same data

--- src/dist_chan/mod.rs
+++ src/dist_chan/mod.rs
@@ -1,42 +1,22 @@
 use std::net::SocketAddr;
 use crossbeam_channel::{Receiver, Sender};
-use mio::{Interest, Poll, Token, net::TcpStream};
+use std::net::TcpStream;
+use std::io::prelude::*;
 
-pub struct BiDirectionalChannel<T: serde::Serialize + Send> {
-    conn: TcpStream,
-    incoming: Receiver<T>,
-    outgoing: Sender<T>,
+pub fn connect_to(ip_addr: SocketAddr) -> Result<TcpStream, std::io::Error> {
+    Ok(TcpStream::connect(ip_addr)?)
 }
 
-impl<T: 'static + serde::Serialize + Send> BiDirectionalChannel<T> {
-    pub fn from_channel(ip_addr: SocketAddr, (send, recv): (Sender<T>, Receiver<T>)) -> Result<Self, std::io::Error> {
-        let poll = Poll::new()?;
-        let listener: Token = Token(ip_addr.port() as usize);
+pub fn produce<T: serde::ser::Serialize>(chan: Receiver<T>, mut conn: TcpStream) {
+    use std::io::Write;
 
-        let mut stream = Self::connect_to(ip_addr)?;
-        poll.registry().register(&mut stream, listener, Interest::READABLE | Interest::WRITABLE)?;
-
-
-        // let write_ref = std::thread::spawn(move || { Self::produce(recv, &mut stream) });
-
-
-        Ok(BiDirectionalChannel {
-            conn: stream,
-            incoming: recv,
-            outgoing: send
-        })
+    for t in chan.iter() {
+        let data = bincode::serialize(&t).ok().expect("serializable data to successfully serialize");
+        conn.write_all(&data).ok();
     }
+}
 
-    fn connect_to(ip_addr: SocketAddr) -> Result<TcpStream, std::io::Error> {
-        TcpStream::connect(ip_addr)
-    }
-
-    fn produce(chan: Receiver<T>, conn: &mut TcpStream) {
-        use std::io::Write;
-
-        for t in chan.iter() {
-            let data = bincode::serialize(&t).ok().expect("serializable data to successfully serialize");
-            conn.write(&data).ok();
-        }
-    }
+pub fn consume<T: for<'de> serde::de::Deserialize<'de>>(chan: Sender<T>, conn: TcpStream) {
+    let data = bincode::deserialize_from(conn).ok().unwrap();
+    chan.send(data).ok().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 pub mod dist_chan;
 
 use dist_chan::*;
+use crossbeam_channel::{Receiver, Sender};
 
 fn main() {
-    let d_chan: BiDirectionalChannel<String> = BiDirectionalChannel::from_channel("127.0.0.1:5678".parse().unwrap(), crossbeam_channel::unbounded()).ok().unwrap();
+    let (outgoing_sender, outgoing_receiver): (Sender<String>, Receiver<String>) = crossbeam_channel::unbounded();
+    let (incoming_sender, incoming_receiver): (Sender<String>, Receiver<String>) = crossbeam_channel::unbounded();
 
-    println!("Hello, world!");
+    let read_conn = connect_to("127.0.0.1:5678".parse().unwrap()).ok().unwrap();
+    let write_conn = read_conn.try_clone().ok().unwrap();
+
+    std::thread::spawn(move || {
+        produce(outgoing_receiver.clone(), write_conn)
+    });
+
+    std::thread::spawn(move || {
+        consume(incoming_sender.clone(), read_conn)
+    });
+
+    outgoing_sender.send(String::from("Hello, world!")).ok().unwrap();
+    for data in incoming_receiver.iter() {
+        println!("{}", data);
+    }
 }
