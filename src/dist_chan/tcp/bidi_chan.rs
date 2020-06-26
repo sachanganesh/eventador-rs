384812476878553e590779164ccd2a638cfcae6a
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2020-06-25 18:01:05 -0700

    solve incomplete buffer problem, so data is collected until it's able to be deserialized

--- src/dist_chan/tcp/bidi_chan.rs
+++ src/dist_chan/tcp/bidi_chan.rs
@@ -1,20 +1,15 @@
-use std::io::Write;
-use std::time::SystemTime;
-
 use async_std::prelude::*;
 use async_std::io::*;
 use async_std::net::*;
 use async_std::task;
 use crossbeam_channel::{Receiver, Sender, unbounded, bounded};
 
-pub const MAX_MESSAGES: usize = 100;
-
 pub struct BiDirectionalTcpChannel<T>
     where T: Send + Sync + serde::ser::Serialize + for<'de> serde::de::Deserialize<'de> {
     rx_chan: (Sender<T>, Receiver<T>),
     tx_chan: (Sender<T>, Receiver<T>),
-    pub reader:  task::JoinHandle<Vec<SystemTime>>,
-    pub writer:  task::JoinHandle<Vec<SystemTime>>,
+    pub reader:  task::JoinHandle<anyhow::Result<()>>,
+    pub writer:  task::JoinHandle<anyhow::Result<()>>,
 }
 
 impl<T> BiDirectionalTcpChannel<T>
@@ -45,11 +40,6 @@ where T: 'static + Send + Sync + serde::ser::Serialize + for<'de> serde::de::Des
 
         write_stream.set_nodelay(true)?;
 
-        // let read_stream = std::net::TcpStream::connect(ip_addrs)?;
-        // let write_stream = read_stream.try_clone()?;
-        // let write_stream = TcpStream::from(write_stream);
-
-
         let _receiver = outgoing_chan.1.clone();
         let _sender   = incoming_chan.0.clone();
 
@@ -74,101 +64,72 @@ where T: 'static + Send + Sync + serde::ser::Serialize + for<'de> serde::de::Des
         task::block_on(self.writer.cancel());
     }
 
-    async fn read_from_stream(mut input: TcpStream, output: Sender<T>) -> Vec<SystemTime>
+    async fn read_from_stream(mut input: TcpStream, output: Sender<T>) -> anyhow::Result<()>
     where T: for<'de> serde::de::Deserialize<'de> {
+        use std::convert::TryInto;
         use bytes::{Buf, BytesMut};
 
-        let mut arr: Vec<SystemTime> = Vec::with_capacity(MAX_MESSAGES);
-        let mut i = 0;
+        const BUFFER_SIZE: usize = 8192;
 
         let mut buffer = BytesMut::new();
-        buffer.resize(8192, 0);
+        buffer.resize(BUFFER_SIZE, 0);
 
-        while let Ok(bytes_read_raw) = input.read(&mut buffer).await {
-            let mut bytes_read: u64 = bytes_read_raw as u64;
+        let mut pending: Option<BytesMut> = None;
+
+        while let Ok(mut bytes_read_raw) = input.read(&mut buffer).await {
+            if let Some(mut pending_buf) = pending.take() {
+                bytes_read_raw += pending_buf.len();
+
+                pending_buf.unsplit(buffer);
+                buffer = pending_buf;
+            }
+
+            let mut bytes_read: u64 = bytes_read_raw.try_into()?;
             while bytes_read > 0 {
-                // print!("r");
-                // std::io::stdout().flush();
-
-                if let Ok(data) = bincode::deserialize(&buffer) {
-                    if let Ok(serialized_size_64) = bincode::serialized_size(&data) {
-                        let serialized_size = serialized_size_64 as usize;
-                        buffer.advance(serialized_size);
-
-                        bytes_read -= serialized_size_64; // overflow errors!! @todo
-                        buffer.resize(8192, 0);
-                    } else {
-                        break;
-                    }
-
-                    arr.push(SystemTime::now());
-                    if let Err(_) = output.try_send(data) {
-                        break;
-                    } else {
-                        i += 1;
-
-                        if i == MAX_MESSAGES {
+                match bincode::deserialize(&buffer) {
+                    Ok(data) => {
+                        if let Ok(serialized_size) = bincode::serialized_size(&data) {
+                            buffer.advance(serialized_size.try_into()?);
+                            bytes_read = bytes_read.saturating_sub(serialized_size);
+                        } else {
                             break;
                         }
+
+                        if let Err(err) = output.send(data) {
+                            return Err(anyhow::Error::from(err))
+                        }
+                    },
+
+                    Err(_err) => {
+                        pending = Some(buffer);
+                        buffer = BytesMut::new();
+                        break;
                     }
                 }
             }
 
-            // task::yield_now().await
-
-            if i == MAX_MESSAGES {
-                break;
-            }
+            buffer.resize(BUFFER_SIZE, 0);
         }
 
-        return arr;
+        Ok(())
     }
 
-    async fn write_to_stream(input: Receiver<T>, mut output: TcpStream) -> Vec<SystemTime>
+    async fn write_to_stream(input: Receiver<T>, mut output: TcpStream) -> anyhow::Result<()>
     where T: serde::ser::Serialize {
-        const MAX_REDUCTIONS: usize = 2000;
-        let mut reductions = 0;
-
-        let mut arr: Vec<SystemTime> = Vec::with_capacity(MAX_MESSAGES);
-        let mut i = 0;
-
         loop {
             while let Ok(t) = input.try_recv() {
-                reductions += 1;
-
-                // print!("w"); //, bincode::serialized_size(&t).expect("todo"));
-                // std::io::stdout().flush();
-
-                arr.push(SystemTime::now());
                 if let Ok(data) = bincode::serialize(&t) {
-                    if let Err(_) = output.write_all(&data).await {
-                        break;
+                    if let Ok(_) = output.write_all(&data).await {
+                        output.flush();
                     } else {
-                        i += 1;
-
-                        if i == MAX_MESSAGES {
-                            break;
-                        }
+                        break;
                     }
                 } else {
                     break;
                 }
-
-                output.flush();
-
-                if reductions == MAX_REDUCTIONS {
-                    reductions = 0;
-                    break;
-                }
-            }
-
-            // task::yield_now().await
-
-            if i == MAX_MESSAGES {
-                break;
             }
         }
 
-        return arr;
+        Ok(())
     }
 }
