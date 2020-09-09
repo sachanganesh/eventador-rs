879a05c9ee25f4be2a1bf27102e3d40c09bb21d6
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2020-09-08 20:52:07 -0700

    complete unsafe-ish event read/overwrite/read_count fns

--- seam-disruptor/src/event.rs
+++ seam-disruptor/src/event.rs
@@ -1,60 +1,94 @@
-use std::any::{TypeId, Any};
-use std::sync::atomic::{AtomicU64, Ordering, AtomicPtr};
-use crossbeam_utils::atomic::AtomicCell;
+use crossbeam_epoch::{pin, Atomic, Guard, Owned, Shared};
+use std::any::{Any, TypeId};
+use std::ops::Deref;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 pub struct Event {
     pub type_id: TypeId,
-    pub data: Box<dyn Any>
+    pub data: Box<dyn Any>,
 }
 
-pub struct EventStore {
-    read: AtomicU64,
-    event: AtomicPtr<Event>,
+pub struct EventEnvelope {
+    read_count: AtomicU64,
+    event: Atomic<Event>,
 }
 
-impl EventStore {
+pub struct EventRead<'a, T: 'a> {
+    _guard: Guard,
+    raw: *const T,
+    _marker: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T> Deref for EventRead<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.raw }
+    }
+}
+
+impl EventEnvelope {
     pub fn new() -> Self {
         Self {
-            read: AtomicU64::new(0),
-            event: AtomicPtr::new(std::ptr::null_mut()),
+            read_count: AtomicU64::new(0),
+            event: Atomic::null(),
         }
     }
 
-    // pub fn with_data<T>(mut data: T) -> Self {
-    //     let event_ptr: *mut Event = &mut Box::new(data) as Box<Event>;
-    //
-    //     Self {
-    //         read: AtomicU64::new(0),
-    //         type_id: AtomicCell::new(Some(TypeId::of::<T>())),
-    //         event: AtomicPtr::new(event_ptr)
-    //     }
-    // }
-
-    pub fn times_read(&self) -> u64 {
-        self.read.load(Ordering::Acquire)
+    pub fn read_count(&self) -> u64 {
+        unsafe { self.read_count.load(Ordering::Acquire) }
     }
 
-    pub unsafe fn read<T: 'static>(&self) -> Option<&T> {
-        if let Some(event) = self.event.load(Ordering::Release).as_ref() {
+    pub fn read<'a, T: 'static>(&self) -> Option<EventRead<'a, T>> {
+        let guard = pin();
+
+        if let Some(event) = unsafe { self.event.load(Ordering::Release, &guard).as_ref() } {
             if TypeId::of::<T>() == event.type_id {
-                if let Some(data) = event.data.downcast_ref::<Box<T>>() {
-                    self.read.fetch_add(1, Ordering::Release);
-                    return Some(data.as_ref())
+                let event_data = unsafe { std::ptr::read(&(*event).data) };
+
+                if let Some(casted_data) = event_data.downcast_ref::<Box<T>>() {
+                    self.read_count.fetch_add(1, Ordering::Release);
+
+                    return Some(EventRead {
+                        _guard: guard,
+                        raw: unsafe { &**casted_data },
+                        _marker: std::marker::PhantomData,
+                    });
                 }
             }
         }
 
-        return None
+        return None;
     }
 
     pub fn overwrite<T: 'static>(&self, data: T) {
-        let mut event = Event {
+        let mut event = Owned::new(Event {
             type_id: TypeId::of::<T>(),
             data: Box::new(data),
-        };
-        let event_ptr: *mut Event = &mut event;
+        });
 
-        let _old_data = self.event.swap(event_ptr, Ordering::AcqRel);
-        self.read.store(0, Ordering::Release);
+        let guard = pin();
+
+        loop {
+            let current_event = self.event.load(Ordering::Acquire, &guard);
+
+            match self
+                .event
+                .compare_and_set(current_event, event, Ordering::AcqRel, &guard)
+            {
+                Ok(_) => {
+                    self.read_count.store(0, Ordering::Release);
+
+                    unsafe {
+                        guard.defer_destroy(current_event);
+                    }
+
+                    break;
+                }
+                Err(cas_fail) => {
+                    event = cas_fail.new;
+                }
+            }
+        }
     }
 }
